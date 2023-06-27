abd91aa77cbe3d13cddf2f129808bc2db11b5502
luckyjim <jcolley@lpnhe.in2p3.fr>
2023-06-27 14:34:16 +0200

    test mc

--- compute/numba/grayscott_numba.py
+++ compute/numba/grayscott_numba.py
@@ -32,8 +32,6 @@ SVML State, config.USING_SVML                 : True
 SVML Library Loaded                           : True
 llvmlite Using SVML Patched LLVM              : True
 SVML Operational                              : True
-
-
 """
 import time
 
@@ -45,12 +43,15 @@ import compute.common as gsc
 
 kwd = {"cache": True, "fastmath": {"reassoc", "contract", "arcp"}}
 
+
 @nb.njit(**kwd)
 def grayscott_numba(m_U, m_V, Du, Dv, Feed, Kill, delta_t, nb_frame, step_frame):
+    # Init constante
     n_x, n_y = m_U.shape[0], m_U.shape[1]
     alpha_u = -Feed - 4 * Du
     alpha_v = -Feed - Kill - 4 * Dv
-    dtFeed = delta_t * Feed 
+    dtFeed = delta_t * Feed
+    # alloc
     frames_V = np.empty((nb_frame, n_x, n_y), dtype=m_V.dtype)
     range_i = range(1, n_x - 1)
     range_j = range(1, n_y - 1)
@@ -64,29 +65,105 @@ def grayscott_numba(m_U, m_V, Du, Dv, Feed, Kill, delta_t, nb_frame, step_frame)
             for li in range_i:
                 for lj in range_j:
                     uvv = c_U[li, lj] * (c_V[li, lj] ** 2)
+                    # update m_U
                     dudt = ((c_U[li, lj] * alpha_u) - uvv) + Du * (
                         c_U[li - 1, lj] + c_U[li + 1, lj] + c_U[li, lj - 1] + c_U[li, lj + 1]
                     )
-                    m_U[li, lj] += ((delta_t * dudt) + dtFeed)
+                    m_U[li, lj] += (delta_t * dudt) + dtFeed
+                    # update m_V
                     dvdt = ((c_V[li, lj] * alpha_v) + uvv) + Dv * (
                         c_V[li - 1, lj] + c_V[li + 1, lj] + c_V[li, lj - 1] + c_V[li, lj + 1]
                     )
                     m_V[li, lj] += delta_t * dvdt
-        frames_V[idx_f,:,:] = m_V
+        frames_V[idx_f, :, :] = m_V
     return frames_V
 
+
+#@nb.njit(**kwd)
+def grayscott_vec_fma_add(m_U, m_V, Du, Dv, Feed, Kill, delta_t, nb_frame, step_frame):
+    # Init constante
+    n_x, n_y = m_U.shape[0], m_U.shape[1]
+    qy_4 = (n_y - 2) // 4
+    ry_4 = (n_y - 2) % 4
+    alpha_u = -Feed - 4 * Du
+    alpha_v = -Feed - Kill - 4 * Dv
+    dtFeed = delta_t * Feed
+    # alloc
+    frames_V = np.empty((nb_frame, n_x, n_y), dtype=m_V.dtype)
+    range_i = range(1, n_x - 1)
+    range_j4 = range(1, n_y - 5, 4)
+    range_4 = range(4)
+    range_step = range(step_frame)
+    c_U = np.empty_like(m_U)
+    c_V = np.empty_like(m_U)
+    # U array for vectorial calculation
+    vec_u1 = np.zeros((8, 4), dtype=m_V.dtype)
+    vec_u2 = np.zeros((3, 4), dtype=m_V.dtype)
+    vec_u1[4] = Dv
+    vec_u1[6] = alpha_u
+    vec_u2[0] = dtFeed
+    vec_u2[1] = delta_t
+    # V array for vectorial calculation
+    vec_v1 = np.zeros((8, 4), dtype=m_V.dtype)
+    vec_v1[4] = Dv
+    vec_v1[6] = alpha_v
+    for idx_f in range(nb_frame):
+        for _ in range_step:
+            c_U[:, :] = m_U
+            c_V[:, :] = m_V
+            for li in range_i:
+                for lj4 in range_j4:
+                    for lk in range_4:
+                        lj = lj4 + lk
+                        uvv = c_U[li, lj] * (c_V[li, lj] ** 2)
+                        # fill vec_u
+                        vec_u1[7, lk] = -uvv
+                        vec_u1[0, lk] = c_U[li - 1, lj]
+                        vec_u1[1, lk] = c_U[li + 1, lj]
+                        vec_u1[5, lk] = c_U[li, lj]
+                        vec_u1[2, lk] = c_U[li, lj - 1]
+                        vec_u1[3, lk] = c_U[li, lj + 1]
+                        # fill vec_v
+                        vec_v1[7, lk] = uvv
+                        vec_v1[0, lk] = c_V[li - 1, lj]
+                        vec_v1[1, lk] = c_V[li + 1, lj]
+                        vec_v1[5, lk] = c_V[li, lj]
+                        vec_v1[2, lk] = c_V[li, lj - 1]
+                        vec_v1[3, lk] = c_V[li, lj + 1]
+                    # vectorial calculuation for m_U
+                    vec_u1[1] += vec_u1[0]
+                    vec_u1[2] += vec_u1[1]
+                    vec_u1[3] += vec_u1[2]
+                    vec_u1[5] = vec_u1[5] * vec_u1[6] + vec_u1[7]
+                    vec_u1[5] += vec_u1[3] * vec_u1[4]
+                    # do m_U[li, lj] += (delta_t * dudt) + dtFeed
+                    m_U[li, lj4 : lj4 + 4] = (
+                        c_U[li, lj4 : lj4 + 4] + vec_u2[0] + vec_u1[5] * vec_u2[1]
+                    )
+                    # vectorial calculuation for m_V
+                    vec_v1[1] += vec_v1[0]
+                    vec_v1[2] += vec_v1[1]
+                    vec_v1[3] += vec_v1[2]
+                    vec_v1[5] = vec_v1[5] * vec_v1[6] + vec_v1[7]
+                    vec_v1[5] += vec_v1[3] * vec_v1[4]
+                    # do m_V[li, lj] += (delta_t * dvdt)
+                    # we used dt vec of U vec_u2[ 1]
+                    m_V[li, lj4 : lj4 + 4] = c_V[li, lj4 : lj4 + 4] + vec_v1[5] * vec_u2[1]
+        frames_V[idx_f, :, :] = m_V
+    return frames_V
 
 
 def grayscott_loop(U, V, nb_frame, step_frame=34):
     Du, Dv = 0.1, 0.05
     F, k = 0.0565, 0.062
-    #F, k = 0.03, 0.062
+    # F, k = 0.03, 0.062
     step_frame = 34
     delta_t = 1
     print(f"step_frame={step_frame}")
     print(f"nb_frame={nb_frame}")
     t0 = time.process_time()
     frames_V = grayscott_numba(U, V, Du, Dv, F, k, delta_t, nb_frame, step_frame)
+    #frames_V = grayscott_vec_fma_add(U, V, Du, Dv, F, k, delta_t, nb_frame, step_frame)
     duration = time.process_time() - t0
     print(f"CPU time= {duration} s")
     frames_ui = np.empty((nb_frame, U.shape[0], U.shape[1]), dtype=np.uint8)
@@ -99,11 +176,11 @@ def grayscott_loop(U, V, nb_frame, step_frame=34):
 
 
 if __name__ == "__main__":
-    n_size = 300
+    n_size = 500
     U, V, _ = gsc.init_gray_scott(n_size, n_size)
-    #U, V, _ = gsc.init_gray_scott(1920, 1080)
-    frames = grayscott_loop(U, V, 100)
-    gsc.frames_to_video(frames, "gray_scott_numba_test")
+    U, V, _ = gsc.init_gray_scott(1920, 1080)
+    frames = grayscott_loop(U, V, 1000)
+    gsc.frames_to_video(frames, "gray_scott_numba_full_1000")
     # last image
     plt.figure()
     plt.imshow(frames[-1])
---
+++ compute/numba_thread/grayscott_mcore.py
@@ -0,0 +1,83 @@
+"""
+
+"""
+import time
+
+import numpy as np
+import matplotlib.pylab as plt
+from numba import njit, prange, threading_layer, set_num_threads
+
+import compute.common as gsc
+
+kwd = {"cache": True, "fastmath": {"reassoc", "contract", "arcp"}, "parallel": True}
+#kwd = {"cache": True, "fastmath": {"reassoc", "contract", "arcp"}}
+
+#@nb.njit(parallel=True, fastmath=True)
+@njit(**kwd)
+def grayscott_mcore(m_U, m_V, Du, Dv, Feed, Kill, delta_t, nb_frame, step_frame):
+    # Init constante
+    n_x, n_y = m_U.shape[0], m_U.shape[1]
+    alpha_u = -Feed - 4 * Du
+    alpha_v = -Feed - Kill - 4 * Dv
+    dtFeed = delta_t * Feed
+    # alloc
+    frames_V = np.empty((nb_frame, n_x, n_y), dtype=m_V.dtype)
+    range_i = range(1, n_x - 1)
+    range_j = range(1, n_y - 1)x
+    range_step = range(step_frame)
+    c_U = np.empty_like(m_U)
+    c_V = np.empty_like(m_U)
+    for idx_f in range(nb_frame):
+        for _ in range_step:
+            c_U[:, :] = m_U
+            c_V[:, :] = m_V            
+            for li in prange(1, n_x - 1):
+                for lj in range_j:
+                    uvv = c_U[li, lj] * (c_V[li, lj] ** 2)
+                    # update m_U
+                    dudt = ((c_U[li, lj] * alpha_u) - uvv) + Du * (
+                        c_U[li - 1, lj] + c_U[li + 1, lj] + c_U[li, lj - 1] + c_U[li, lj + 1]
+                    )
+                    m_U[li, lj] += (delta_t * dudt) + dtFeed
+                    # update m_V
+                    dvdt = ((c_V[li, lj] * alpha_v) + uvv) + Dv * (
+                        c_V[li - 1, lj] + c_V[li + 1, lj] + c_V[li, lj - 1] + c_V[li, lj + 1]
+                    )
+                    m_V[li, lj] += delta_t * dvdt
+        frames_V[idx_f, :, :] = m_V
+    return frames_V
+
+
+def grayscott_loop(U, V, nb_frame, step_frame=34):
+    Du, Dv = 0.1, 0.05
+    F, k = 0.0565, 0.062
+    # F, k = 0.03, 0.062
+    step_frame = 34
+    delta_t = 1
+    print(f"step_frame={step_frame}")
+    print(f"nb_frame={nb_frame}")
+    t0 = time.process_time()
+    frames_V = grayscott_mcore(U, V, Du, Dv, F, k, delta_t, nb_frame, step_frame)
+    duration = time.process_time() - t0
+    print(f"CPU time= {duration} s")
+    print(threading_layer())
+    frames_ui = np.empty((nb_frame, U.shape[0], U.shape[1]), dtype=np.uint8)
+    for idx in range(nb_frame):
+        V = frames_V[idx]
+        V_min = V.min()
+        V_scaled = np.uint8(255 * (V - V_min / (V.max() - V_min)))
+        frames_ui[idx] = V_scaled
+    return frames_ui
+
+
+if __name__ == "__main__":
+    set_num_threads(4)
+    #n_size = 500
+    #U, V, _ = gsc.init_gray_scott(n_size, n_size)
+    U, V, _ = gsc.init_gray_scott(1920, 1080)
+    frames = grayscott_loop(U, V, 1000)
+    gsc.frames_to_video(frames, "gray_scott_mcore_full_4c")
+    # last image
+    # plt.figure()
+    # plt.imshow(frames[-1])
+    # plt.show()
